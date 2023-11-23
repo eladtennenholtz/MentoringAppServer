c966920ebadcbaac360ff16ec21f71c1d5594af3
eladtennenholtz <eladtennenholtz1@gmail.com>
2023-11-23 23:08:29 +0200

    set socket listeners after db connection

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const mongoose = require("mongoose");
 
 const app = express();
 app.use(cors());
-//sdf
 
 const server = http.createServer(app);
 const io = new Server(server, {
@@ -16,7 +15,7 @@ const io = new Server(server, {
   },
 });
 
-async function connectToMongoDB() {
+async function connectToMongoDB(callback) {
   try {
     console.log("connectToMongoDB -> start");
     await mongoose.connect(
@@ -37,109 +36,113 @@ async function connectToMongoDB() {
 
     if (!databaseExists) {
       console.log("connectToMongoDB -> db not exists");
-
       await adminDb.createDatabase("codeBlocksDB");
     }
     console.log("Connected to MongoDB");
+
+    // Notify that MongoDB is connected
+    callback();
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
   }
 }
 
 // Call the async function to connect to MongoDB
-await connectToMongoDB();
+connectToMongoDB(() => {
+  // MongoDB is connected, set up Socket.IO events
 
-const CodeBlockSchema = new mongoose.Schema({
-  id: String,
-  title: String,
-  code: String,
-  createdBy: String,
-  updatedAt: { type: Date, default: Date.now },
-});
+  const CodeBlockSchema = new mongoose.Schema({
+    id: String,
+    title: String,
+    code: String,
+    createdBy: String,
+    updatedAt: { type: Date, default: Date.now },
+  });
 
-const CodeBlock = mongoose.model("CodeBlock", CodeBlockSchema);
+  const CodeBlock = mongoose.model("CodeBlock", CodeBlockSchema);
 
-const connectedClients = [];
+  const connectedClients = [];
 
-// Function to emit all code blocks to a specific client
-const emitAllCodeBlocksToClient = async (socket) => {
-  try {
-    // Query the database to get all code blocks
-    console.log("emitAllCodeBlocksToClient");
-    const allCodeBlocks = await CodeBlock.find();
-    // Send the retrieved code blocks to the client
-    socket.emit("all_code_blocks", allCodeBlocks);
-  } catch (error) {
-    console.error("Error getting all code blocks:", error);
-  }
-};
-
-io.on("connection", (socket) => {
-  connectedClients.push(socket.id);
-  const roleClient = connectedClients.length === 1 ? "mentor" : "student";
-  socket.emit("role", { clientId: socket.id, role: roleClient });
-
-  // Listen for the "send_initial_code_blocks" event
-  socket.on("send_initial_code_blocks", async (initialCodeBlocks) => {
-    // Save the received code blocks to the database
+  // Function to emit all code blocks to a specific client
+  const emitAllCodeBlocksToClient = async (socket) => {
     try {
-      for (const block of initialCodeBlocks) {
-        // Check if the code block already exists in the database
-        const existingCodeBlock = await CodeBlock.findOne({ id: block.id });
+      // Query the database to get all code blocks
+      console.log("emitAllCodeBlocksToClient");
+      const allCodeBlocks = await CodeBlock.find();
+      // Send the retrieved code blocks to the client
+      socket.emit("all_code_blocks", allCodeBlocks);
+    } catch (error) {
+      console.error("Error getting all code blocks:", error);
+    }
+  };
 
-        if (!existingCodeBlock) {
-          const codeBlock = new CodeBlock({
-            id: block.id,
-            title: block.title,
-            code: block.code,
-            createdBy: socket.id, // Update this as needed
-            updatedAt: Date.now(),
-          });
+  io.on("connection", (socket) => {
+    connectedClients.push(socket.id);
+    const roleClient = connectedClients.length === 1 ? "mentor" : "student";
+    socket.emit("role", { clientId: socket.id, role: roleClient });
 
-          await codeBlock.save();
+    // Listen for the "send_initial_code_blocks" event
+    socket.on("send_initial_code_blocks", async (initialCodeBlocks) => {
+      // Save the received code blocks to the database
+      try {
+        for (const block of initialCodeBlocks) {
+          // Check if the code block already exists in the database
+          const existingCodeBlock = await CodeBlock.findOne({ id: block.id });
+
+          if (!existingCodeBlock) {
+            const codeBlock = new CodeBlock({
+              id: block.id,
+              title: block.title,
+              code: block.code,
+              createdBy: socket.id, // Update this as needed
+              updatedAt: Date.now(),
+            });
+
+            await codeBlock.save();
+          }
         }
+      } catch (error) {
+        console.error("Error saving initial code blocks:", error);
       }
-    } catch (error) {
-      console.error("Error saving initial code blocks:", error);
-    }
-  });
+    });
 
-  socket.on("get_all_code_blocks", async () => {
-    console.log("get_all_code_blocks -> start");
+    socket.on("get_all_code_blocks", async () => {
+      console.log("get_all_code_blocks -> start");
 
-    // Emit all code blocks to the connected user
-    emitAllCodeBlocksToClient(socket);
-  });
+      // Emit all code blocks to the connected user
+      emitAllCodeBlocksToClient(socket);
+    });
 
-  socket.on("send_message", async (data) => {
-    try {
-      // Update the code block in the database
-      await CodeBlock.findOneAndUpdate(
-        { id: data.id },
-        { code: data.message, updatedAt: Date.now() }
-      );
-    } catch (error) {
-      console.error("Error updating code block:", error);
-    }
+    socket.on("send_message", async (data) => {
+      try {
+        // Update the code block in the database
+        await CodeBlock.findOneAndUpdate(
+          { id: data.id },
+          { code: data.message, updatedAt: Date.now() }
+        );
+      } catch (error) {
+        console.error("Error updating code block:", error);
+      }
 
-    // Broadcast the updated code block to all connected clients
-    socket.broadcast.emit("receive_message", {
-      message: data.message,
-      id: data.id,
+      // Broadcast the updated code block to all connected clients
+      socket.broadcast.emit("receive_message", {
+        message: data.message,
+        id: data.id,
+      });
+    });
+
+    // Handle user disconnection
+    socket.on("disconnect", () => {
+      const index = connectedClients.indexOf(socket.id);
+      if (index !== -1) {
+        connectedClients.splice(index, 1);
+      }
     });
   });
 
-  // Handle user disconnection
-  socket.on("disconnect", () => {
-    const index = connectedClients.indexOf(socket.id);
-    if (index !== -1) {
-      connectedClients.splice(index, 1);
-    }
+  const PORT = process.env.PORT || 3001;
+
+  server.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
   });
-});
-
-const PORT = process.env.PORT || 3001;
-
-server.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
 });
